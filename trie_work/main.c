40490a8c04dc4942dca9e49837aa0ff7fb757b9d
Joseph Julicher <jjulicher@mac.com>
2018-12-19 21:23:31 -0700

    Initial Commit

---
+++ trie_work/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  trie_work
+//
+//  Created by Joe Julicher on 12/19/18.
+//  Copyright © 2018 Joe Julicher. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
